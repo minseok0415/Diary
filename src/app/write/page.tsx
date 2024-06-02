bfbaf5b50f11f61c9c94dd50bd0d883ddb52b62b
minseok0415 <kmskorea14@gmail.com>
2024-06-02 14:22:31 +0900

    FEAT: 일기 쓰기 페이지 구조 설정

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,6 +1,20 @@
 const Write = () => {
     return (
         <>
+            <div>
+                <div>
+                    <input type="text" />
+                    <input type="date"  />
+                </div>
+                <div>
+                    그림 그리는 자리
+                </div>
+                <textarea />
+            </div>
+            <div>
+                <button type="submit">저장</button>
+                <button type="button">취소</button>
+            </div>
         </>
     )
 }
