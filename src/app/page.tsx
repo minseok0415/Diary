0c2e4c1cf18e5bdfaf54e1486a177d07bc6a76c1
minseok0415 <kmskorea14@gmail.com>
2024-06-02 14:11:43 +0900

    FEAT: 홈페이지 구조 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-const App = () => {
+import Link from "next/link"
+
+const Home = () => {
   return (
     <>
+      <h1>나의 그림일기</h1>
+      <Link href="/calendar">
+        일기 목록
+      </Link>
+      <Link href="/write">
+        오늘 일기 쓰기
+      </Link>
     </>
   )
 }
 
-export default App
+export default Home
