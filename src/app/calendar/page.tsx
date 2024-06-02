61e22d03fd1984d7ea30f6927df642e993681d18
minseok0415 <kmskorea14@gmail.com>
2024-06-02 22:05:38 +0900

    FEAT: 달력 페이지 일기 불러오기 기능 추가

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,4 +1,29 @@
+"use client"
+import { useEffect } from "react"
+
 const Calendar = () => {
+    useEffect(() => {
+        getDiary()
+    }, [])
+
+    const getDiary = async () => {
+        await fetch("http://localhost:3000/api", {
+            method: "GET"
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return response.json()
+                })
+                .then(data => {
+                    console.log("Response data:", data)
+                })
+                .catch(error => {
+                    console.error("Error:", error)
+                })
+    }
+
     return (
         <>
         </>
