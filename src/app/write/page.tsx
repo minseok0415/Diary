7a8db66ebc1d1b792ad07dbb8bec088d160cc02b
minseok0415 <kmskorea14@gmail.com>
2024-06-02 15:12:51 +0900

    FEAT: 날짜 기본값 오늘로 설정

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,12 +1,27 @@
+"use client"
+import { useEffect, useState } from "react"
 import Canvas from "@/components/canvas"
 
 const Write = () => {
+    const [currentDate, setCurrentDate] = useState("")
+
+    useEffect(() => {
+        const today = new Date()
+        const year = today.getFullYear()
+        const month = String(today.getMonth() + 1).padStart(2, '0')
+        const day = String(today.getDate()).padStart(2, '0')
+        const daysOfWeek = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
+        const dayOfWeek = daysOfWeek[today.getDay()]
+        const formattedDate = `${year}년 ${month}월 ${day}일 ${dayOfWeek}`
+        setCurrentDate(formattedDate)
+    }, [])
+    
     return (
         <>
             <div>
                 <div>
                     <input type="text" />
-                    <p>오늘 날짜</p>
+                    <p>{currentDate}</p>
                 </div>
                 <Canvas />
                 <textarea />
