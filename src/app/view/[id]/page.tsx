d0e5c8489979a6f2a853972017695ea63976102f
minseok0415 <kmskorea14@gmail.com>
2024-06-03 12:18:50 +0900

    FEAT: 일기 세부 페이지 api 적용

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -1,9 +1,34 @@
+"use client"
+import { useEffect } from "react"
+
 interface Props {
     params: { id: number },
 }
 
 const Diary = (props: Props) => {
     const diaryID = props.params.id
+
+    useEffect(() => {
+        getDiaryById()
+    }, [])
+
+    const getDiaryById = async () => {
+        await fetch(`http://localhost:3000/api/diary?id=${diaryID}`, {
+            method: "GET"
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return response.json()
+                })
+                .then(data => {
+                    console.log("Response data:", data)
+                })
+                .catch(error => {
+                    console.error("Error:", error)
+                })
+    }
 
     return (
         <>
