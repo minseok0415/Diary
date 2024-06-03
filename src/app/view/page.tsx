f5426055981ef0cf8c4451a0bddc506c2754cf87
minseok0415 <kmskorea14@gmail.com>
2024-06-03 09:55:52 +0900

    REFACTOR: 달력 컴포넌트 분리 및 api 코드 이동

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,36 +1,12 @@
 "use client"
-import { useEffect } from "react"
 import Link from "next/link"
+import Calendar from "@/components/calendar"
 
 const View = () => {
-    useEffect(() => {
-        getDiary()
-    }, [])
-
-    const getDiary = async () => {
-        await fetch("http://localhost:3000/api", {
-            method: "GET"
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok")
-                }
-                return response.json()
-                })
-                .then(data => {
-                    console.log("Response data:", data)
-                })
-                .catch(error => {
-                    console.error("Error:", error)
-                })
-    }
-
     return (
         <>
             <h1>일기 목록</h1>
-            <div>
-                달력 위치
-            </div>
+            <Calendar />
             <Link href="/">홈으로</Link>
             <Link href="/write">오늘 일기 쓰기</Link>
         </>
---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+
+const Calendar = () => {
+    useEffect(() => {
+        getDiary()
+    }, [])
+
+    const getDiary = async () => {
+        await fetch("http://localhost:3000/api", {
+            method: "GET"
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok")
+                }
+                return response.json()
+                })
+                .then(data => {
+                    console.log("Response data:", data)
+                })
+                .catch(error => {
+                    console.error("Error:", error)
+                })
+    }
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Calendar
