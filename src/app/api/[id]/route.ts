79803524ca4bd7df083c8d36fa99fbd86a7e0853
minseok0415 <kmskorea14@gmail.com>
2024-06-03 14:13:21 +0900

    FEAT: 일기 수정 api 추가

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -53,3 +53,33 @@ export async function DELETE(request: NextRequest) {
     status: 200,
   })
 }
+
+export async function PUT(request: NextRequest) {
+    if (!db) {
+      db = await open({
+        filename: "./diary.db",
+        driver: sqlite3.Database,
+      })
+    }
+
+    const diaryID = request.nextUrl.searchParams.get('id')
+  
+    const { title, content, date, canvasData } = await request.json()
+  
+    try {
+        await db.run(
+            "UPDATE diary SET title = ?, content = ?, date = ?, canvasData = ? WHERE id = ?",
+            [title, content, date, canvasData, diaryID]
+        )
+  
+      return new Response(JSON.stringify({ message: "Item updated successfully" }), {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      })
+    } catch (error: any) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        headers: { "Content-Type": "application/json" },
+        status: 500,
+      })
+    }
+  }
