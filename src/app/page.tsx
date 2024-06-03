0c9dfcffcace0082988bb405f8c1a52b2d42af4e
minseok0415 <kmskorea14@gmail.com>
2024-06-03 19:35:59 +0900

    DESIGN: 글로벌 스타일 생성 및 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client"
 import Link from "next/link"
+import styled, { createGlobalStyle } from "styled-components"
 
 const Home = () => {
+  const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+    }
+  `
+
   return (
     <>
+      <GlobalStyle />
       <h1>나의 그림일기</h1>
       <Link href="/view">
         일기 목록
